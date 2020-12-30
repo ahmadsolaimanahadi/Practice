bdfdfe9b0e81dcabad2374072eb994c5f11cf49a
solaiman_98 <65265684+ahmadsolaimanahadi@users.noreply.github.com>
2020-12-30 12:49:33 -0500

    halo halo

---
+++ src/Part1/Git.java
@@ -0,0 +1,18 @@
+package Part1;
+
+public class Git {
+	
+	static int currentYear;
+	static int bornYear;
+	static int age;
+	
+	public static void main(String[] args) {
+		int currentYear = 2021;
+		int bornYear = 1998;
+		int age = (currentYear - bornYear);
+		
+		System.out.println(age);
+		
+	}
+
+}
